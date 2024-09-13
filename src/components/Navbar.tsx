60cd5ae4cc381ac89d463370e596260b15a8554f
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2024-09-13 15:26:50 -0500

    added assests on connect page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,17 @@ const Navbar = () => {
       setIsMenuOpen(!isMenuOpen);
    };
 
-  // TODO: Replace "#" with actual links
-  const menuItems = [
-    { name: "about", link: "/about" },
-    { name: "officers", link: "/officers" },
-    { name: "events", link: "/events" },
-    { name: "connect", link: "/connect" },
-    { name: "apply", link: "#" },
-  ];
+   // TODO: Replace "#" with actual links
+   const menuItems = [
+      { name: "about", link: "/about" },
+      { name: "officers", link: "/officers" },
+      { name: "events", link: "/events" },
+      { name: "connect", link: "/connect" },
+      {
+         name: "apply",
+         link: "https://mavorgs.campuslabs.com/engage/organization/acm",
+      },
+   ];
 
    return (
       <nav className="backdrop-blur-md bg-white/10 border-gray-200 sticky top-0 z-50">
